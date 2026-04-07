4cc7dbb5f313e5accf8ad6aed0bbc8d0fafe65b4
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-04-07 21:10:31 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   type        = string
   description = "Name of the existing Resource Group"
-  default     = "1-9afd2763-playground-sandbox"
+  default     = "1-bdec5174-playground-sandbox"
   }
 
 variable "location" {
