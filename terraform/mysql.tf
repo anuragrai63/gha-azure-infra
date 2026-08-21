c30e8bf2e2f383ce93f36afdaf7122eb8e571f75
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-21 18:14:01 +0530

    Update mysql.tf

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -1,23 +1,19 @@
-resource "random_string" "mysql" {
-  length  = 5
-  special = false
-  upper   = false
-}
+
 
 resource "azurerm_private_dns_zone" "mysql" {
-  name                = "mysql.database.azure.com"
+  name                = "gmvmysql2108.private.database.azure.com"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "mysql" {
-  name                  = "mysql${random_string.mysql.result}"
+  name                  = "gmvmysql2108"
   private_dns_zone_name = azurerm_private_dns_zone.mysql.name
   resource_group_name   = data.azurerm_resource_group.rg.name
   virtual_network_id    = azurerm_virtual_network.main.id
 }
 
 resource "azurerm_mysql_flexible_server" "mysql" {
-  name                   = "mysql${random_string.mysql.result}"
+  name                   = "gmvmysql2108"
   resource_group_name    = data.azurerm_resource_group.rg.name
   location               = data.azurerm_resource_group.rg.location
   depends_on = [azurerm_private_dns_zone_virtual_network_link.mysql]
