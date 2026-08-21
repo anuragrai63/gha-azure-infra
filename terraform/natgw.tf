9b8dde220c4f67e83b4b36d05561c7fa406b059d
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-21 16:27:40 +0530

    Add NAT Gateway and associated resources in Terraform

---
+++ terraform/natgw.tf
@@ -0,0 +1,26 @@
+resource "azurerm_public_ip" "nat" {
+  name                = "nat-public-ip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+resource "azurerm_nat_gateway" "nat" {
+  name                = "app-nat-gateway"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  sku_name = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "nat" {
+  nat_gateway_id       = azurerm_nat_gateway.nat.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "app" {
+  subnet_id      = azurerm_subnet.app.id
+  nat_gateway_id = azurerm_nat_gateway.nat.id
+}
