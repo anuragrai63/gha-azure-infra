f5e154df216241459c4362f7c8f6de619c907930
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-21 16:30:36 +0530

    Add Azure Application Gateway resource configuration

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -25,3 +25,60 @@ resource "azurerm_web_application_firewall_policy" "waf" {
     }
   }
 }
+
+resource "azurerm_application_gateway" "agw" {
+
+  name                = "appgateway"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  firewall_policy_id = azurerm_web_application_firewall_policy.waf.id
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "gateway-ip"
+    subnet_id = azurerm_subnet.agw.id
+  }
+
+  frontend_port {
+    name = "port80"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = "public"
+    public_ip_address_id = azurerm_public_ip.agw.id
+  }
+
+  backend_address_pool {
+    name = "vmsspool"
+  }
+
+  backend_http_settings {
+    name                  = "http-setting"
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+  }
+
+  http_listener {
+    name                           = "listener"
+    frontend_ip_configuration_name = "public"
+    frontend_port_name             = "port80"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = "rule1"
+    rule_type                  = "Basic"
+    http_listener_name         = "listener"
+    backend_address_pool_name  = "vmsspool"
+    backend_http_settings_name = "http-setting"
+    priority                   = 100
+  }
+}
