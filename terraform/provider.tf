b8e06bd297b24abb414fb15fd80e5a9753a656f3
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-21 16:24:55 +0530

    Upgrade Terraform and azurerm provider versions
    Updated required version for Terraform and providers.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,18 @@
   terraform {
+  required_version = ">=1.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
+tls = {
+  source = "hashicorp/tls"
+}
+random = {
+   source = "hashicorp/random"
+}
+
   }
 
   backend "azurerm" {
@@ -14,4 +23,5 @@
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+subscription_id = "9734ed68-621d-47ed-babd-269110dbacb1"
 }
