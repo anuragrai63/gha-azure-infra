298e878fec83199857f0fa8fd13c61dbf0019c4f
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-21 16:27:09 +0530

    Add Azure Bastion host and public IP resources

---
+++ terraform/bastion.tf
@@ -0,0 +1,20 @@
+resource "azurerm_public_ip" "bastion" {
+  name                = "bastion-pub-ip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "my-bastion"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
+}
