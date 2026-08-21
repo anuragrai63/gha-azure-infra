1b7e1939a344ca45e53a6eb9fc570a137603084c
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-21 16:29:28 +0530

    Create gateway.tf

---
+++ terraform/gateway.tf
@@ -0,0 +1,27 @@
+resource "azurerm_public_ip" "agw" {
+  name                = "agw-public-ip"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+}
+
+resource "azurerm_web_application_firewall_policy" "waf" {
+
+  name                = "appgw-waf-policy"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+
+  policy_settings {
+    enabled = true
+    mode    = "Prevention"
+  }
+
+  managed_rules {
+    managed_rule_set {
+      type    = "OWASP"
+      version = "3.2"
+    }
+  }
+}
