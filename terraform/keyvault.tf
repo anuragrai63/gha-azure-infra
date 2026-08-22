dd201a1a3979b7967aa87b7a026472bcfdbc2702
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-22 15:20:24 +0530

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,12 +1,15 @@
 resource "azurerm_key_vault" "kv" {
-
   name                = "anurag-kv-demo-eastus"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-
-  sku_name = "standard"
+  sku_name  = "standard"
 }
 
+resource "azurerm_key_vault_secret" "ssh_private_key" {
+  name         = "azureuser-private-key"
+  value        = file("${path.module}/azureuser-priv-key.pem")
+  key_vault_id = azurerm_key_vault.kv.id
+}
 
