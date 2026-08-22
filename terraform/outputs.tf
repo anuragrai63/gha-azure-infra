fe3d4a300f611e1b72967bbe33024c577ba537cd
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-22 15:05:18 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,12 +2,5 @@ output "bastion_public_ip" {
   value = azurerm_public_ip.bastion.ip_address
 }
 
-output "application_gateway_public_ip" {
-  value = azurerm_public_ip.agw.ip_address
-}
-
-output "mysql_server_name" {
-  value = azurerm_mysql_flexible_server.mysql.name
-}
 
 
