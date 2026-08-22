c4db6fa7916d509e7df98e2d35621b3a853618cc
Anurag Rai <66570766+anuragrai63@users.noreply.github.com>
2026-08-22 15:53:17 +0530

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -7,9 +7,33 @@ resource "azurerm_key_vault" "kv" {
   sku_name  = "standard"
 }
 
+resource "azurerm_key_vault_access_policy" "terraform" {
+  key_vault_id = azurerm_key_vault.kv.id
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  object_id = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Purge"
+  ]
+}
+
+
+
+
+
+
 resource "azurerm_key_vault_secret" "ssh_private_key" {
   name         = "azureuser-private-key"
   value        = file("${path.module}/azureuser-priv-key.pem")
   key_vault_id = azurerm_key_vault.kv.id
-}
 
+  depends_on = [
+    azurerm_key_vault_access_policy.terraform
+  ]
+}
